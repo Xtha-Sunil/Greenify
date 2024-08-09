7fe61320312dc34ac9d1231c6457cb180558571b
Sunil Shrestha <116238476+Xtha-Sunil@users.noreply.github.com>
2024-08-09 19:35:33 +0545

    Update greener.py

--- greener.py
+++ greener.py
@@ -2,17 +2,27 @@ from github import Github
 from datetime import datetime
 import os
 
+# Get today's date in the format YYYY-MM-DD
 today_date = datetime.today().strftime("%Y-%m-%d")
 
-g = Github(os.getenv('api_key'))
+# Authenticate with GitHub using the API key stored in environment variables
+g = Github(os.getenv('API_KEY'))
 
-repo = g.get_repo("Xtha-Sunil/Greenify")
+# Retrieve the authenticated user's username
+user = g.get_user()
+username = user.login
 
+# Get the repository object for the user's 'Greenify' repository
+repo = g.get_repo(f"{username}/Greenify")
+
+# Loop to create 8 new files in the repository
 for i in range(8):
+    # Define the file path and content
     file_path = f"{today_date}/new{i}.txt"
     file_content = f"file {i + 1}"
     
     try:
+        # Attempt to create a new file in the repository
         repo.create_file(
             path=file_path,
             message="Creating a new file",
@@ -20,4 +30,5 @@ for i in range(8):
         )
         print(f"Created file: {file_path}")
     except Exception as e:
+        # Handle any errors that occur during file creation
         print(f"Failed to create file: {file_path}. Error: {e}")
